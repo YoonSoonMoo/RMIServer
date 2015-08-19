964e5f0eeed9e6ee22b01c25e88a0b6161fbb118
YoonSoonMoo <ysmb482@gmail.com>
2015-08-19 17:20:32 +0900

    Enum정의 클래스

---
+++ rmServer/src/daou/platform/main/Const.java
@@ -0,0 +1,18 @@
+package daou.platform.main;
+
+public class Const {
+
+	/**
+	 * 로그의 레벨 정의
+	 * @author yoon
+	 */
+	public enum LOG_LEVEL {
+		//로그레벨 디버그
+		DEBUG,
+		//로그레벨 정보
+		INFO,
+		//로그레벨 에러
+		ERROR;
+	}
+	
+}
