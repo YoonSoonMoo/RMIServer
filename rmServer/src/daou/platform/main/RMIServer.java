dc8418b50fb17018462d680ccbce23b6c39b1bd0
YoonSoonMoo <ysmb482@gmail.com>
2015-08-19 14:43:09 +0900

    로그설정

--- rmServer/src/daou/platform/main/RMIServer.java
+++ rmServer/src/daou/platform/main/RMIServer.java
@@ -1,5 +1,7 @@
 package daou.platform.main;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,10 +14,15 @@ import daou.platform.service.RmiService;
  */
 public class RMIServer {
 
+	private static Log log = LogFactory.getLog(RMIServer.class);
+	
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 		RmiService service = context.getBean("rmiServiceImpl",RmiService.class);
+		log.info("Log4j Server Start");
 		System.out.println("Server Start!!!!!");
+		
+		
 	}
 }
